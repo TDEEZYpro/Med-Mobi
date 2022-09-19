11dd4a0fbe41cc73680ccf914d9a99981f262c7d
TDEEZYpro <nkosinathimnguni2016@gmail.com>
2022-09-20 00:11:26 +0200

    testing

---
+++ Med-Mobi.py
@@ -0,0 +1,96 @@
+#Training Mobi
+from ast import pattern
+#import selenium
+import numpy as np
+import nltk
+import string
+import random
+import json
+import pickle
+
+from nltk.stem import WordNetLemmatizer
+from keras.models import Sequential #imports sequential model to train Med-Mo
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+
+nltk.download('punkt')
+nltk.download('wordnet')
+
+words = []
+classes = []
+documents = []
+words_ignore = []
+intent_data = open('C:\Users\tdeez\Documents\Med-Mobi\intents.json', encoding='utf-8').read()
+intents = json.load(intent_data)
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        w= nltk.word_tokenize(pattern)
+        words.extend(w)
+        documents.append((w, intent['tag']))
+
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in words_ignore]
+words = sorted(list(set(words)))
+
+classes = sorted(list(set(classes)))
+
+print (len(documents), "documents")
+
+#To create a document with a list of all the classes which are going to be the tags in our intents
+print (len(classes), "classes", classes)
+
+#Lemmatize is breaking down words to basically their natural or  basic version e.g liar, lied, lies all have a bases of lie 
+print (len(words), "unique lemmatized words", words)
+
+
+pickle.dump(words,open('words.pkl','wb'))
+pickle.dump(classes,open('classes.pkl','wb'))
+
+# initializing training, bot associate certain words with a tag thus resulting in it learning when a certain word is used it classifies it with another to undrrstand better
+training = []
+output_empty = [0] * len(classes)
+for doc in documents:
+
+    bag = []
+
+    pattern_words = doc[0]
+    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
+
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
+
+
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+
+    training.append([bag, output_row])
+
+random.shuffle(training)
+training = np.array(training)
+# create train and test lists. X - patterns, Y - intents
+train_x = list(training[:,0])
+train_y = list(training[:,1])
+print("Training data created")
+
+
+# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
+# equal to number of intents to predict output intent with softmax
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+#fitting and saving the model
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', hist)
+ 
+print("Model has been creatted")
+
